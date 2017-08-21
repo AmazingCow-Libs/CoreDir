91c35095ce90c3c52171867bc2cc08a0d2d3d8e4
n2omatt <n2o.matt@gmail.com>
2017-08-20 21:21:24 -0300

    - Add license header and cosmetic changes.

--- include/CoreDir.h
+++ include/CoreDir.h
@@ -1,4 +1,43 @@
-//COWTODO: Add license header.
+//----------------------------------------------------------------------------//
+//               █      █                                                     //
+//               ████████                                                     //
+//             ██        ██                                                   //
+//            ███  █  █  ███        CoreDir.h                                 //
+//            █ █        █ █        CoreDir                                   //
+//             ████████████                                                   //
+//           █              █       Copyright (c) 2017                        //
+//          █     █    █     █      AmazingCow - www.AmazingCow.com           //
+//          █     █    █     █                                                //
+//           █              █       N2OMatt - n2omatt@amazingcow.com          //
+//             ████████████         www.amazingcow.com/n2omatt                //
+//                                                                            //
+//                  This software is licensed as GPLv3                        //
+//                 CHECK THE COPYING FILE TO MORE DETAILS                     //
+//                                                                            //
+//    Permission is granted to anyone to use this software for any purpose,   //
+//   including commercial applications, and to alter it and redistribute it   //
+//               freely, subject to the following restrictions:               //
+//                                                                            //
+//     0. You **CANNOT** change the type of the license.                      //
+//     1. The origin of this software must not be misrepresented;             //
+//        you must not claim that you wrote the original software.            //
+//     2. If you use this software in a product, an acknowledgment in the     //
+//        product IS HIGHLY APPRECIATED, both in source and binary forms.     //
+//        (See opensource.AmazingCow.com/acknowledgment.html for details).    //
+//        If you will not acknowledge, just send us a email. We'll be         //
+//        *VERY* happy to see our work being used by other people. :)         //
+//        The email is: acknowledgment_opensource@AmazingCow.com              //
+//     3. Altered source versions must be plainly marked as such,             //
+//        and must not be misrepresented as being the original software.      //
+//     4. This notice may not be removed or altered from any source           //
+//        distribution.                                                       //
+//     5. Most important, you must have fun. ;)                               //
+//                                                                            //
+//      Visit opensource.amazingcow.com for more open-source projects.        //
+//                                                                            //
+//                                  Enjoy :)                                  //
+//----------------------------------------------------------------------------//
+
 #pragma once
 
 //std
@@ -19,7 +58,6 @@ bool CreateDirectory(const std::string &path, unsigned mode = 0777);
 bool Delete(const std::string &path, bool recursive = false);
 
 
-
 //Returns the names of the subdirectories (including their paths)
 //that match the specified search pattern in the specified directory,
 //and optionally searches subdirectories.
@@ -27,7 +65,6 @@ std::vector<std::string> GetDirectories(
         const std::string &path,
         const std::string &pattern   = ".*",
         bool               recursive = true);
-
 
 //Returns the names of files (including their paths) that match the
 //specified search pattern in the specified directory, using a value
@@ -51,6 +88,5 @@ bool Exists(const std::string &path);
 
 //Moves a file or a directory and its contents to a new location.
 bool Move(const std::string &src, const std::string &dst);
-
 
 NS_COREDIR_END
